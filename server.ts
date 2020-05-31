13308ead8d7e614cdc1e1ccc8d1bae1939386fa3
Matti Petrelius <matti.petrelius@gmail.com>
2020-05-31 18:33:24 +0300

    Change app to use Heroku port variable

--- server.ts
+++ server.ts
@@ -50,6 +50,7 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5000, function () {
-  console.log("App is listening on port 5000!");
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log(`App is listening on port ${port}!`);
 });
